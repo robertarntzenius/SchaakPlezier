9bb13b2048b1e434902be47d4689d6be1e45e175
Luuk Blom <ljcluukb@gmail.com>
2024-02-17 16:26:56 +0100

    implement profiler in cmake & buildandrun refactor buildandrun

--- BuildAndRun.sh
+++ BuildAndRun.sh
@@ -1,98 +1,174 @@
 #!/bin/bash
 
-build_type="Release"
-verbose=false
-rebuild=false
-execute=false
+######### Functions ######### 
+function validate_build_type {
+  if [ "$build_type" != "Release" ] && [ "$build_type" != "Debug" ] && [ "$build_type" != "Verbose" ]; then
+      echo "Invalid build type, use Debug or Release. (Build type: $build_type)"
+      exit 1
+  fi
+}
 
-# Parse all args here using getopts
-while getopts "hb:rve" opt; do
-  case $opt in
-    h) # Show usage
-      echo "Usage: ./BuildAndRun.sh [OPTIONS]"
-      echo ""
-      echo "Options:"
-      echo "  -b <BUILDTYPE>   Set the build type to either 'Debug' or 'Release'. Default is 'Release'."
-      echo "  -r               Rebuild from scratch."
-      echo "  -e               Execute SchaakPlezier after building."
-      echo "  -v               Display the log file content after running."
-      echo "  -h               Display this message."
-      exit
-      ;;
-    r) # rebuild
+function clean_build_dir {
+  if [ -d "$build_dir" ] && [ $rebuild == true ]; then
+      echo "Rebuild flag is set. Deleting build directory: $build_dir"
+      rm -r "$build_dir"
+  fi
+}
+
+function set_profiler_opt {
+  profiler_opt="OFF"
+  if [ $profiler == true ]; then
       rebuild=true
-      ;;
-    v) # cat the logfile if you add -v to this script call
-      verbose=true
-      ;;
-    e) # run ./Schaakplezier after building
-      execute=true
-      ;;
-    b) # set the build type using -b Debug or -b Release
-      build_type="${OPTARG}"
-      ;;
-    \?)
-      echo "Invalid option: $opt"
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument."
-      exit 1
-      ;;
-  esac
-done
+      profiler_opt="ON"
+      echo "Profiler flag is set. Compiling with symbols"
+  fi
+}
 
-shift $((OPTIND-1))
+function run_cmake_make {
+  set_profiler_opt
+  
+  mkdir -p "$build_dir" || exit
+  cmake -S "$source_dir" -B "$build_dir" -DBUILD_TYPE="$build_type" -DPROFILER="$profiler_opt"
+  cd "$build_dir"
 
-# Check for any remaining arguments (unsupported options)
-if [ "$#" -gt 0 ]; then
-    echo "Unsupported option(s) provided: $@"
-    exit
-fi
+  make || exit
+}
 
-# Set the build type
-if [ "$build_type" != "Release" ] && [ "$build_type" != "Debug" ] && [ "$build_type" != "Verbose" ]; then
-    echo "Invalid build type, use Debug or Release. (Build type: $build_type)"
-    exit 1
-fi
+function run_ctest {
+  if [ "$build_type" == "Debug" ] || [ "$build_type" == "Verbose" ]; then
+      echo ""
+      echo ""
+      echo ""
+      echo "===== Tests ====="
+      ctest --rerun-failed --output-on-failure
+      echo ""
+  fi
+}
 
-source_dir="$PWD/core"
-build_dir="$PWD/build/$build_type"
-
-# Clean the build directory
-if [ -d "$build_dir" ] && [ $rebuild == true ]; then
-    echo "Rebuild flag is set. Deleting build directory: $build_dir"
-    rm -r "$build_dir"
-fi
-
-mkdir -p "$build_dir" || exit
-cmake -S "$source_dir" -B "$build_dir" -DBUILD_TYPE="$build_type"
-cd "$build_dir"
-
-make || exit
-
-# Test if Debug or Verbose
-if [ "$build_type" == "Debug" ] || [ "$build_type" == "Verbose" ]; then
+function run_profiler {
+  if [ $profiler == true ]; then
     echo ""
     echo ""
     echo ""
-    echo "===== Tests ====="
-    ctest --rerun-failed --output-on-failure
+    echo "===== Profiler ====="
     echo ""
-fi
 
-if $execute; then 
-  ./SchaakPlezier -w human -b random || exit
-fi
+    local executable="$1"
+    if [ ! -x "$executable" ]; then
+        echo "Executable not found or not executable: $executable"
+        exit 1
+    fi
+    local valgrind_output="callgrind.out"
 
-# Output log to terminal if verbose
-if $verbose; then
-    echo ""
-    echo ""
-    echo ""
-    echo "===== Schaakplezier.log ====="
-    echo ""
-    cat "$build_dir/Schaakplezier.log"
-fi
+    valgrind --tool=callgrind --callgrind-out-file="$valgrind_output" "$executable"
+    kcachegrind "$valgrind_output"
+  fi
+}
 
-cd "$source_dir" || exit
+function run_SchaakPlezier {
+  if $execute; then 
+    ./SchaakPlezier -w $1 -b $2 || exit
+  fi
+}
+
+function cat_logfile {
+  if $verbose; then
+      echo ""
+      echo ""
+      echo ""
+      echo "===== Schaakplezier.log ====="
+      echo ""
+      cat "$build_dir/Schaakplezier.log"
+  fi
+}
+
+function parse_args {
+  while getopts "hb:rveg" opt; do
+    case $opt in
+      h) # Show usage
+        show_usage
+        exit
+        ;;
+      r) # rebuild
+        rebuild=true
+        ;;
+      v) # cat the logfile
+        verbose=true
+        ;;
+      e) # run ./Schaakplezier after building
+        execute=true
+        ;;
+      g) # compile with -g, run valgrind profiler & analyze
+        profiler=true
+        ;;
+      b) # set the build type using -b Debug or -b Release
+        build_type="${OPTARG}"
+        ;;
+      \?)
+        echo "Invalid option: $opt"
+        exit 1
+        ;;
+      :)
+        echo "Option -$OPTARG requires an argument."
+        exit 1
+        ;;
+    esac
+  done
+  shift $((OPTIND-1))
+
+  # Remaining arguments
+  if [ "$#" -gt 0 ]; then
+      echo "Unsupported option(s) provided: $@"
+      exit
+  fi
+}
+
+function show_usage {
+  echo "Usage: ./BuildAndRun.sh [OPTIONS]"
+  echo ""
+  echo "Options:"
+  echo "  -b <BUILDTYPE>   Set the build type to either 'Debug' or 'Release'. Default is 'Release'."
+  echo "  -r               Rebuild from scratch."
+  echo "  -e               Execute SchaakPlezier after building."
+  echo "  -g               Compile with debugging symbols to enable profiler."
+  echo "  -v               Display the log file content after running."
+  echo "  -h               Display this message."
+}
+
+function main {
+  # Set default variables
+  build_type="Release"
+  verbose=false
+  rebuild=false
+  execute=false
+  profiler=false
+
+  # User input
+  parse_args "$@"
+
+  # Set build & source dirs
+  validate_build_type
+  source_dir="$PWD/core"
+  build_dir="$PWD/build/$build_type"
+
+  # Compile
+  clean_build_dir
+  run_cmake_make
+
+  # Test & Analyze
+  run_ctest
+  # run_profiler "$build_dir/board/test/test_movegeneration"
+  run_profiler "$build_dir/SchaakPlezier"
+
+  # Run
+  run_SchaakPlezier random random
+
+  # Output
+  cat_logfile
+
+  # Reset working directory
+  cd "$source_dir" || exit
+}
+
+######### Main #########
+main "$@"
