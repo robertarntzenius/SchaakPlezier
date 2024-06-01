60a3f351067a29ec5906ea526c6e8f4ce19c22ec
Luuk Blom <ljcluukb@gmail.com>
2024-06-01 11:44:22 +0200

    cleanup wrapper types

--- core/pygui/model/wrapper_types.py
+++ core/pygui/model/wrapper_types.py
@@ -1,5 +1,82 @@
 import wrappers
-from enum import Enum
+
+class CppWrapperEnum():
+    name: str
+    value: int
+
+    _item_to_string: dict[int, str]
+    _string_to_item: dict[str, int]
+        
+    def __init__(self, _cppEnumName: str, value_or_name: int | str):
+        self._name: str = _cppEnumName
+        cpp_enum = getattr(wrappers, _cppEnumName)
+
+        self.wrapper_items = cpp_enum.__members__.values()
+        self._item_to_string = {item.value: item.name for item in self.wrapper_items}
+        self._string_to_item = {item.name: item.value for item in self.wrapper_items}
+
+        if isinstance(value_or_name, str):
+            self.name = value_or_name
+            self.value = self.name_to_value(value_or_name)
+        elif isinstance(value_or_name, int):
+            self.value = value_or_name
+            self.name = self.value_to_name(value_or_name)
+        elif isinstance(value_or_name, cpp_enum):
+            self.name = value_or_name.name
+            self.value = value_or_name.value
+        else:
+            raise ValueError(f"Invalid input for {self._name}")
+
+    def value_to_name(self, value: int):
+        if value not in self._item_to_string:
+            raise ValueError(f"Invalid {self._name} value: {value}")
+        return self._item_to_string[value]
+    
+    def name_to_value(self, name: str):
+        if name not in self._string_to_item:
+            raise ValueError(f"Invalid {self._name} name: {name}")
+        return self._string_to_item[name]
+    
+    def __eq__(self, other) -> bool:
+        if isinstance(other, int):
+            return self.value == other
+        elif isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, CppWrapperEnum) and self._name == other._name:
+            return self.value == other.value
+        else:
+            return False
+    
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self) -> str:
+        return f"{self._name}.{self.name}"
+
+
+class Square(CppWrapperEnum):
+    def __init__(self, value_or_name: int | str):
+        super().__init__('Square', value_or_name)
+
+
+class Piecetype(CppWrapperEnum):
+    def __init__(self, value_or_name: int | str):
+        super().__init__('Piecetype', value_or_name)
+
+
+class Color(CppWrapperEnum):
+    def __init__(self, value_or_name: int | str):
+        super().__init__('Color', value_or_name)
+
+
+class GameResult(CppWrapperEnum):
+    def __init__(self, value_or_name: int | str):
+        super().__init__('GameResult', value_or_name)
+
+
+class PlayerType(CppWrapperEnum):
+    def __init__(self, value_or_name: int | str):
+        super().__init__('PlayerType', value_or_name)
 
 
 class Move:
@@ -60,232 +137,3 @@ class Move:
     
     def __repr__(self):
         return f"{self.fromSquare.name}{self.targetSquare.name} - {self.playerPiece.name}"
-
-
-class Square:
-    squares = wrappers.Square.__members__.values()
-    _Square = Enum('Square', {square.name: square.value for square in squares})
-    _square_to_string = {square.value: square.name for square in squares}
-    _string_to_square = {square.name: square.value for square in squares}
-
-    value: int
-    name: str
-
-    def __init__(self, value_or_name):
-        if isinstance(value_or_name, str):
-            self.name = value_or_name
-            self.value = self.name_to_value(value_or_name)
-        elif isinstance(value_or_name, int):
-            self.value = value_or_name
-            self.name = self.value_to_name(value_or_name)
-        elif isinstance(value_or_name, wrappers.Square):
-            self.name = value_or_name.name
-            self.value = value_or_name.value
-        else:
-            raise ValueError("Invalid input for Square")
-
-    def __eq__(self, other) -> bool:
-        if isinstance(other, int):
-            return self.value == other
-        elif not isinstance(other, Square):
-            raise NotImplementedError(f"Dont compare {type(self).__name__} with {type(other).__name__}")
-        return self.value == other.value
-
-    def __repr__(self) -> str:
-        return self.name
-
-    @staticmethod
-    def value_to_name(value: int):
-        if value not in Square._square_to_string:
-            raise ValueError(f"Invalid square value: {value}")
-        return Square._square_to_string[value]
-    
-    @staticmethod
-    def name_to_value(name: str):
-        if name not in Square._string_to_square:
-            raise ValueError(f"Invalid square name: {name}")
-        return Square._string_to_square[name]
-    
-    
-class Piecetype:
-    piecetypes = wrappers.Piecetype.__members__.values()
-    _Piecetype = Enum('Piecetype', {piecetype.name: piecetype.value for piecetype in piecetypes})
-    _piecetype_to_string = {piecetype.value: piecetype.name for piecetype in piecetypes}
-    _string_to_piecetype = {piecetype.name: piecetype.value for piecetype in piecetypes}
-
-    name: str
-    value: int
-
-    def __init__(self, value_or_name):
-        if isinstance(value_or_name, str):
-            self.name = value_or_name
-            self.value = self.name_to_value(value_or_name)
-        elif isinstance(value_or_name, int):
-            self.value = value_or_name
-            self.name = self.value_to_name(value_or_name)
-        elif isinstance(value_or_name, wrappers.Piecetype): 
-            self.value = value_or_name.value
-            self.name = value_or_name.name
-        else:
-            raise ValueError("Invalid input for Piecetype")
-    
-    def to_cpp_object(self):
-        return wrappers.string_piecetype_map()[self.name.lower()]
-
-    def __eq__(self, other) -> bool:
-        if isinstance(other, int):
-            return self.value == other
-        elif not isinstance(other, Square):
-            raise NotImplementedError(f"Dont compare {type(self).__name__} with {type(other).__name__}")
-        return self.value == other.value
-
-    def __repr__(self) -> str:
-        return self.name
-
-    @staticmethod
-    def value_to_name(value: int):
-        if value not in Piecetype._piecetype_to_string:
-            raise ValueError(f"Invalid piecetype value: {value}")
-        return Piecetype._piecetype_to_string[value]
-    
-    @staticmethod
-    def name_to_value(name: str):
-        if name not in Piecetype._string_to_piecetype:
-            raise ValueError(f"Invalid piecetype name: {name}")
-        return Piecetype._string_to_piecetype[name]
-    
-
-class Color:
-    colors = wrappers.Color.__members__.values()
-    _Color = Enum('Color', {color.name: color.value for color in colors})
-    _color_to_string = {color.value: color.name for color in colors}
-    _string_to_color = {color.name: color.value for color in colors}
-
-    name: str
-    value: int
-    
-    def __init__(self, value_or_name):
-        if isinstance(value_or_name, str):
-            self.name = value_or_name
-            self.value = self.name_to_value(value_or_name)
-        elif isinstance(value_or_name, int):
-            self.value = value_or_name
-            self.name = self.value_to_name(value_or_name)
-        elif isinstance(value_or_name, wrappers.Color):
-            self.name = value_or_name.name
-            self.value = value_or_name.value
-        else:
-            raise ValueError("Invalid input for Color")
-    
-    def __eq__(self, other) -> bool:
-        if isinstance(other, int):
-            return self.value == other
-        elif not isinstance(other, Color):
-            raise NotImplementedError(f"Dont compare {type(self)} with {type(other)}")
-        return self.value == other.value
-
-    def __repr__(self) -> str:
-        return self.name
-
-    @staticmethod
-    def value_to_name(value: int):
-        if value not in Color._color_to_string:
-            raise ValueError(f"Invalid color value: {value}")
-        return Color._color_to_string[value]
-    
-    @staticmethod
-    def name_to_value(name: str):
-        if name not in Color._string_to_color:
-            raise ValueError(f"Invalid color name: {name}")
-        return Color._string_to_color[name]
-
-
-class GameResult:
-    gameResults = wrappers.GameResult.__members__.values()
-    _GameResult = Enum('GameResult', {result.name: result.value for result in gameResults})
-    _result_to_string = {result.value: result.name for result in gameResults}
-    _string_to_result = {result.name: result.value for result in gameResults}
-
-    name: str
-    value: int
-    
-    def __init__(self, value_or_name):
-        if isinstance(value_or_name, str):
-            self.name = value_or_name
-            self.value = self.name_to_value(value_or_name)
-        elif isinstance(value_or_name, int):
-            self.value = value_or_name
-            self.name = self.value_to_name(value_or_name)
-        elif isinstance(value_or_name, wrappers.GameResult):
-            self.name = value_or_name.name
-            self.value = value_or_name.value
-        else:
-            raise ValueError(f"Invalid input for GameResult: {value_or_name}")
-    
-    def __eq__(self, other) -> bool:
-        if isinstance(other, int):
-            return self.value == other
-        elif not isinstance(other, GameResult):
-            raise NotImplementedError(f"Dont compare {type(self)} with {type(other)}")
-        return self.value == other.value
-
-    def __repr__(self) -> str:
-        return self.name
-
-    @staticmethod
-    def value_to_name(value: int):
-        if value not in GameResult._result_to_string:
-            raise ValueError(f"Invalid result value: {value}")
-        return GameResult._result_to_string[value]
-    
-    @staticmethod
-    def name_to_value(name: str):
-        if name not in GameResult._string_to_result:
-            raise ValueError(f"Invalid result name: {name}")
-        return GameResult._string_to_result[name]
-
-
-class PlayerType:
-    playerTypes = wrappers.PlayerType.__members__.values()
-    _PlayerTypes = Enum('PlayerTypes', {player_type.name: player_type.value for player_type in playerTypes})
-    _result_to_string = {player_type.value: player_type.name for player_type in playerTypes}
-    _string_to_result = {player_type.name: player_type.value for player_type in playerTypes}
-    
-    name: str
-    value: int
-    
-    def __init__(self, value_or_name):
-        if isinstance(value_or_name, str):
-            self.name = value_or_name
-            self.value = self.name_to_value(value_or_name)
-        elif isinstance(value_or_name, int):
-            self.value = value_or_name
-            self.name = self.value_to_name(value_or_name)
-        elif isinstance(value_or_name, wrappers.PlayerType):
-            self.name = value_or_name.name
-            self.value = value_or_name.value
-        else:
-            raise ValueError(f"Invalid input for PlayerType: {value_or_name}")
-    
-    def __eq__(self, other) -> bool:
-        if isinstance(other, int):
-            return self.value == other
-        elif not isinstance(other, PlayerType):
-            raise NotImplementedError(f"Dont compare {type(self)} with {type(other)}")
-        return self.value == other.value
-
-    def __repr__(self) -> str:
-        return self.name
-    
-    @staticmethod
-    def value_to_name(value: int):
-        if value not in PlayerType._result_to_string:
-            raise ValueError(f"Invalid result value: {value}")
-        return PlayerType._result_to_string[value]
-    
-    @staticmethod
-    def name_to_value(name: str):
-        name = name[0].upper() + name[1:]
-        if name not in PlayerType._string_to_result:
-            raise ValueError(f"Invalid result name: {name}")
-        return PlayerType._string_to_result[name]
