51c03ae30dcbcba79c62cb457d2e6c91a25df6e5
Luuk Blom <ljcluukb@gmail.com>
2024-09-04 00:09:29 +0200

    add bin dir to path no matter what

--- core/schaak_plezier/__init__.py
+++ core/schaak_plezier/__init__.py
@@ -7,6 +7,7 @@ ASSETS_DIR = PROJECT_ROOT / "core" / "schaak_plezier" / "assets"
 BIN_DIR = PROJECT_ROOT / "build" / "bin"
 
 assert BIN_DIR.is_dir(), f"Error: {BIN_DIR} does not exist. Build the backend first by running the build script corresponding to your os, located in {PROJECT_ROOT / 'scripts'}. "
+sys.path.append(str(BIN_DIR))
 
 if os.name == "nt":
     # windows only. source: https://stackoverflow.com/questions/78124769/pybind11-how-do-i-truly-fix-missing-dll-error
@@ -14,7 +15,6 @@ if os.name == "nt":
     MINGW_BIN = Path(r"c:\programdata\mingw64\mingw64\bin").resolve()
     assert MINGW_BIN.is_dir(), f"error: {MINGW_BIN} is not a valid directory."
 
-    sys.path.append(str(BIN_DIR))
     for dir in [BIN_DIR, MINGW_BIN]:
         os.add_dll_directory(str(dir))
 
