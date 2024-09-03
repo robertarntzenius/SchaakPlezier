f2814e5c08a54458126cf84f74a0a6d603ce84a0
LuukB98 <133109180+LuukB98@users.noreply.github.com>
2024-09-03 23:59:40 +0200

    Bash changes (#5)
    * breakup buildandrun into 2 scripts
    
    * fix __init__.py to handle different platforms

--- core/schaak_plezier/__init__.py
+++ core/schaak_plezier/__init__.py
@@ -1,20 +1,22 @@
-import sys
 import os
+import sys
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parents[2]
 ASSETS_DIR = PROJECT_ROOT / "core" / "schaak_plezier" / "assets"
 BIN_DIR = PROJECT_ROOT / "build" / "bin"
 
-assert BIN_DIR.is_dir(), f"Error: {BIN_DIR} is not a valid directory. Build the C++ backend first"
+assert BIN_DIR.is_dir(), f"Error: {BIN_DIR} does not exist. Build the backend first by running the build script corresponding to your os, located in {PROJECT_ROOT / "scripts"}. "
 
-# windows only. source: https://stackoverflow.com/questions/78124769/pybind11-how-do-i-truly-fix-missing-dll-error
-# TODO find out what is needed for linux & find out how to do this without hardcoding a path
-MINGW_BIN = Path(r'c:\programdata\mingw64\mingw64\bin').resolve()
-assert MINGW_BIN.is_dir(), f"error: {MINGW_BIN} is not a valid directory."
+if os.name == "nt":
+    # windows only. source: https://stackoverflow.com/questions/78124769/pybind11-how-do-i-truly-fix-missing-dll-error
+    # TODO find out what is needed for linux & find out how to do this without hardcoding a path
+    MINGW_BIN = Path(r"c:\programdata\mingw64\mingw64\bin").resolve()
+    assert MINGW_BIN.is_dir(), f"error: {MINGW_BIN} is not a valid directory."
 
-sys.path.append(str(BIN_DIR))
-for dir in [BIN_DIR, MINGW_BIN]:
-    os.add_dll_directory(str(dir))
+    sys.path.append(str(BIN_DIR))
+    for dir in [BIN_DIR, MINGW_BIN]:
+        os.add_dll_directory(str(dir))
 
-import wrappers # import this here to fail fast
+# import this here to fail fast
+import wrappers  # noQa
---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+root_dir="$PWD"
+source_dir="$root_dir/core"
+build_dir="$root_dir/build/$build_type"
+
+mkdir -p "$build_dir" || exit
+cmake -S "$source_dir" -B "$build_dir" -DBUILD_TYPE="Release" -DCMAKE_CXX_COMPILER="g++" -DCMAKE_C_COMPILER="gcc"
+cmake --build "$build_dir"
+cmake --install "$build_dir"
+
+if [ ! -d "$root_dir/.venv" ]; then
+  python3 -m venv "$root_dir/.venv"
+fi
+
+source "$root_dir/.venv/bin/activate" && pip install -e "$root_dir" --upgrade
