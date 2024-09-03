1b7d1183b4cde354485cf83c18c9a918c26c2bee
Luuk Blom <ljcluukb@gmail.com>
2024-09-04 00:06:50 +0200

    change quotes to stop error

--- core/schaak_plezier/__init__.py
+++ core/schaak_plezier/__init__.py
@@ -6,7 +6,7 @@ PROJECT_ROOT = Path(__file__).parents[2]
 ASSETS_DIR = PROJECT_ROOT / "core" / "schaak_plezier" / "assets"
 BIN_DIR = PROJECT_ROOT / "build" / "bin"
 
-assert BIN_DIR.is_dir(), f"Error: {BIN_DIR} does not exist. Build the backend first by running the build script corresponding to your os, located in {PROJECT_ROOT / "scripts"}. "
+assert BIN_DIR.is_dir(), f"Error: {BIN_DIR} does not exist. Build the backend first by running the build script corresponding to your os, located in {PROJECT_ROOT / 'scripts'}. "
 
 if os.name == "nt":
     # windows only. source: https://stackoverflow.com/questions/78124769/pybind11-how-do-i-truly-fix-missing-dll-error
