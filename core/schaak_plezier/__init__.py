ccd924c276a1f660e1c7efe34094ed1aa8f27deb
Luuk Blom <ljcluukb@gmail.com>
2024-09-03 23:44:58 +0200

    finished build.bat and run.bat for windows. I used choco and mingw to install build dependencies. Kind of a dirty fix for running in windows. see TODO in toplevel __init__.py

--- core/schaak_plezier/__init__.py
+++ core/schaak_plezier/__init__.py
@@ -1,4 +1,20 @@
+import sys
+import os
 from pathlib import Path
 
-ASSETS_DIR = Path(__file__).parent / "assets"
 PROJECT_ROOT = Path(__file__).parents[2]
+ASSETS_DIR = PROJECT_ROOT / "core" / "schaak_plezier" / "assets"
+BIN_DIR = PROJECT_ROOT / "build" / "bin"
+
+assert BIN_DIR.is_dir(), f"Error: {BIN_DIR} is not a valid directory. Build the C++ backend first"
+
+# windows only. source: https://stackoverflow.com/questions/78124769/pybind11-how-do-i-truly-fix-missing-dll-error
+# TODO find out what is needed for linux & find out how to do this without hardcoding a path
+MINGW_BIN = Path(r'c:\programdata\mingw64\mingw64\bin').resolve()
+assert MINGW_BIN.is_dir(), f"error: {MINGW_BIN} is not a valid directory."
+
+sys.path.append(str(BIN_DIR))
+for dir in [BIN_DIR, MINGW_BIN]:
+    os.add_dll_directory(str(dir))
+
+import wrappers # import this here to fail fast
--- scripts/build.bat
+++ scripts/build.bat
@@ -1,14 +1,16 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 REM ====================
 REM Set Paths. (script_path -> remove trailing `\`, get parent, remove trailing `\`)
 REM ====================
+
 set "script_dir=%~dp0"
 set "script_dir=%script_dir:~0,-1%"
 for %%I in ("%script_dir%") do set "root_dir=%%~dpI"
 set "root_dir=%root_dir:~0,-1%"
-
+set "build_dir=%root_dir%\build\Release"
+set "source_dir=%root_dir%\core"
 
 REM ====================
 REM Create and activate the virtual environment
@@ -20,42 +22,16 @@ if not exist "%root_dir%\.venv" (
 )
 call "%root_dir%\.venv\Scripts\activate.bat"
 
-
 REM ====================
 REM Run CMake and Ninja
 REM ====================
-echo Creating build directory if it does not exist...
 if not exist "%build_dir%" (
+    echo Creating build directory at "%build_dir%" ...
     mkdir "%build_dir%" || exit /b 1
 )
-echo Running CMake...
 cmake -S "%source_dir%" -B "%build_dir%" -DBUILD_TYPE="Release" -DCMAKE_CXX_COMPILER="g++" -DCMAKE_C_COMPILER="gcc" -G Ninja || exit /b 1
-
-echo Running Generator...
-ninja -C "%build_dir%" || exit /b 1
-
-
-REM ====================
-REM Make created binaries available
-REM ====================
-set API_DIR="%build_dir%\api"
-set PATH=%PATH%;%API_DIR%
-
-REM Attempt to import the module to verify the copy
-python -c "import wrappers" 2>stderr.log
-
-REM Check if there were errors
-if exist stderr.log (
-    type stderr.log
-    del stderr.log
-    exit /b 1
-)
-
-REM ====================
-REM Run the GUI
-REM ====================
-echo Running GUI...
-python "%source_dir%\main.py"
+cmake --build "%build_dir%" || exit /b 1
+cmake --install "%build_dir%" || exit /b 1
 
 cd "%root_dir%" || exit /b 1
 
