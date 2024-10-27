01289f93e789d5368b0d9260d151bcde644d6966
Luuk Blom <ljcluukb@gmail.com>
2024-10-27 20:55:43 +0100

    idk how i got these

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,8 @@ source_dir="$root_dir/core"
 build_dir="$root_dir/build/Release"
 pybind11_src="$root_dir/extern/pybind11"
 pybind11_build="$root_dir/extern/pybind11/build"
+pygui_dir="$root_dir/pygui"
+install_dir="$HOME/.local"
 
 git submodule update --init --recursive
 if [ ! -d "$pybind11_build" ]; then
@@ -16,7 +18,7 @@ if [ ! -d "$pybind11_build" ]; then
 fi
 
 mkdir -p "$build_dir" || exit 1
-cmake -S "$source_dir" -B "$build_dir" -DBUILD_TYPE="Release" -DCMAKE_CXX_COMPILER="g++" -DCMAKE_C_COMPILER="gcc" || exit 1
+cmake -S "$source_dir" -B "$build_dir" -DBUILD_TYPE="Release" -DCMAKE_CXX_COMPILER="g++" -DCMAKE_C_COMPILER="gcc" -DCMAKE_INSTALL_PREFIX="$install_dir" || exit 1
 cmake --build "$build_dir" || exit 1
 cmake --install "$build_dir" || exit 1
 
@@ -27,6 +29,6 @@ fi
 
 source "$root_dir/.venv/bin/activate" || exit 1
 
-cd "$build_dir/lib" && stubgen --package wrappers --output "$build_dir/lib" && cd "$root_dir" || exit 1
+cd "$build_dir/lib" && stubgen --package wrappers --output "$install_dir" && cd "$root_dir" || exit 1
 
-pyreverse "$source_dir/schaak_plezier" -o pdf --output-directory "$root_dir/notes" || exit 1
+pyreverse "$pygui_dir" -o pdf --output-directory "$root_dir/notes" || exit 1
